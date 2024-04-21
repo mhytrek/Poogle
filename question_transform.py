288745d571c3ecf6f24ea391d1e873fb450e7de4
Michalina <michalina@iMac-Michalina.local>
2024-04-21 16:06:22 +0200

    Matrix and search

--- question_transform.py
+++ question_transform.py
@@ -1,13 +1,36 @@
 import vectors_of_words
-import numpy
+import numpy as np
+import pickle
 
 def create_vector(content, bag_of_words):
     words = vectors_of_words.find_words(content)
+    print(words)
     vector = [0 for _ in range(len(bag_of_words))]
     for word in words:
         if word in bag_of_words:
             vector[bag_of_words.index(word)] += 1
-    return vector
+    return np.array(vector)
 
-def calc_corelation(content,matrix):
+def calc_corelation(q,A):
+    similarity = q.T @ A
+    return similarity
+
+def get_most_similar(cor, doc,n):
+    result = sorted(doc, key=lambda x: cor[doc.index(x)], reverse=True)
+    for i in range(min(n,len(doc))):
+        print(result[i].get('title'))
+
+
+def search(q):
+    file_handler = open("Data/Articles_test_data", "rb")
+    data = pickle.load(file_handler)
+    bag_of_words = data.get('bagofwords')
+    matrix = data.get('matrix')
+    documents = data.get('articles')
+    q_vec = create_vector(q, bag_of_words)
+    cos = calc_corelation(q_vec, matrix)
+    get_most_similar(cos, documents, 10)
+
+search("What is Polka?")
+
 
--- vectors_of_words.py
+++ vectors_of_words.py
@@ -6,18 +6,19 @@ import numpy as np
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
-nltk.download('stopwords')
+from sklearn.preprocessing import normalize
+# nltk.download('stopwords')
 
 def find_words(content):
     ps = PorterStemmer()
-    words = re.split(r'[\s|,|\.|=|;|"|:|\(|\)|\{|\}|\[|\]]+', content)
-    words = map(ps.stem, words)
+    words = re.split(r'[\s|,|\.|=|;|"|:|\(|\)|\{|\}|\[|\]|?|!]+', content)
+    words = list(map(ps.stem, words))
     return words
 
 def all_files_vector():
     voc = set()
-    for file_name in os.listdir("wiki_articles"):
-        file_path = os.path.join("wiki_articles", file_name)
+    for file_name in os.listdir("test"):
+        file_path = os.path.join("test", file_name)
         with open(file_path, 'rb') as file_opened:
             obj = pickle.load(file_opened)
             content = obj.get('content')
@@ -33,25 +34,39 @@ def find_articles_vector(bag_of_word):
     N = len(bag_of_word)
     matrix = []
     document_freq = [0 for _ in range(len(bag_of_word))]
-    for file_name in os.listdir("wiki_articles"):
+    documents = []
+    for file_name in os.listdir("test"):
         vec = [0 for _ in range(len(bag_of_word))]
-        file_path = os.path.join("wiki_articles", file_name)
+        file_path = os.path.join("test", file_name)
         with open(file_path, 'rb') as file_opened:
             obj = pickle.load(file_opened)
             content = obj.get('content')
             words = find_words(content)
+            documents.append({'title': obj.get('title'), 'url': obj.get('url')})
         for word in words:
             if word in bag_of_word:
-                if vec[bag_of_word.index(word)] == 1:
+                if vec[bag_of_word.index(word)] == 0:
                     document_freq[bag_of_word.index(word)] += 1
                 vec[bag_of_word.index(word)] += 1
         matrix.append(np.array(vec, dtype=object))
-    for col in range(len(bag_of_word[0])):
-        idf = math.log(N/document_freq[col])
-        for i in range(N):
+    for col in range(N):
+        idf = math.log(len(matrix)/document_freq[col])
+        for i in range(len(matrix)):
             matrix[i][col] *= idf
-    pickle.dump(matrix, "Matrix", "wb")
-    return np.array(matrix, dtype=object)
+    matrix = np.array(matrix, dtype=object)
+    normalized_matrix = normalize(matrix,axis=1, norm='l1')
+    normalized_matrix = normalized_matrix.T
+    document_freq = np.array(document_freq)
+
+    file_handler = open("Data/Articles_test_data", "wb")
+    A = {'matrix': normalized_matrix,
+         'frequency': document_freq,
+         'bagofwords': bag_of_word,
+         'articles': documents,
+         }
+    pickle.dump(A, file_handler)
+    file_handler.close()
+    return
 
 
 
@@ -61,6 +76,3 @@ def main():
     return len(bag)
 
 print(main())
-
-
-
